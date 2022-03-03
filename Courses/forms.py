c59877615b63e96448a8878816e25d646aa69384
ifiokLab <100873498+ifiokLab@users.noreply.github.com>
2022-03-03 20:51:28 +0100

    add files via upload

---
+++ Courses/forms.py
@@ -0,0 +1,80 @@
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm,AuthenticationForm
+from django import forms
+from django.utils.translation import gettext_lazy as _
+from django.core.exceptions import ValidationError
+from django.forms import ModelForm
+from .models import Course, Module,myuser
+from django.forms.models import inlineformset_factory
+
+
+ModuleFormSet = inlineformset_factory(Course,Module,fields=['title','description'],extra=2,can_delete=True)
+
+class CustomUserCreationForm(UserCreationForm):
+    #password1 =forms.CharField(max_length=200, widget=forms.PasswordInput(),label='Confirm Password')
+    #password =forms.CharField(max_length=200, widget=forms.PasswordInput(), label='Password')
+
+    def __init__(self, *args, **kwargs):
+        super(CustomUserCreationForm, self).__init__(*args, **kwargs)
+        self.fields['email'].widget.attrs['placeholder'] = self.fields['email'].label
+        self.fields['first_name'].widget.attrs['placeholder'] = self.fields['first_name'].label
+        self.fields['last_name'].widget.attrs['placeholder'] = self.fields['last_name'].label
+        self.fields['password1'].widget.attrs['placeholder'] = self.fields['password1'].label
+        self.fields['password2'].widget.attrs['placeholder'] = self.fields['password2'].label
+
+
+    class Meta:
+        model = myuser
+        fields = ('email','first_name','last_name','Department','password1','password2')
+
+
+class CustomUserChangeForm(UserChangeForm):
+
+    class Meta:
+        model = myuser
+        fields = ('email','first_name','last_name','Department')
+
+class MyLoginForm(AuthenticationForm):
+    username = forms.EmailField(label='Email',widget=forms.TextInput(attrs={'placeholder': 'Email'}))
+    password = forms.CharField(max_length=32, widget=forms.PasswordInput(attrs={'placeholder': 'Password'}))
+
+
+class AdminLoginForm(AuthenticationForm):
+    username = forms.EmailField(label='Email',widget=forms.TextInput(attrs={'placeholder': 'Email'}))
+    password = forms.CharField(max_length=32, widget=forms.PasswordInput(attrs={'placeholder': 'Password'}))
+
+    error_messages = {
+        **AuthenticationForm.error_messages,
+        'invalid_login': _(
+            "Please enter the correct Email and password for a staff "
+            "account. Note that both fields may be case-sensitive."
+        ),
+    }
+    def confirm_login_allowed(self, user):
+        super().confirm_login_allowed(user)
+        if not user.is_staff:
+            raise ValidationError(
+                self.error_messages['invalid_login'],
+                code='invalid_login',
+                params={'username': self.username_field.verbose_name}
+            )
+
+
+class TeacherSignUpForm(UserCreationForm):
+    def __init__(self, *args, **kwargs):
+        super(TeacherSignUpForm, self).__init__(*args, **kwargs)
+        self.fields['email'].widget.attrs['placeholder'] = self.fields['email'].label
+        self.fields['first_name'].widget.attrs['placeholder'] = self.fields['first_name'].label
+        self.fields['last_name'].widget.attrs['placeholder'] = self.fields['last_name'].label
+        self.fields['password1'].widget.attrs['placeholder'] = self.fields['password1'].label
+        self.fields['password2'].widget.attrs['placeholder'] = self.fields['password2'].label
+
+    class Meta:
+        model = myuser
+        fields = ('email','first_name','last_name','Department','password1','password2')
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.is_teacher = True
+        if commit:
+            user.save()
+        return user
